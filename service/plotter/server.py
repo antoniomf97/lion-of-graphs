9b9632d036318b55fc0b0e408edfb358eee4268e
António Fernandes <antonio.mf97@gmail.com>
2022-07-10 16:56:48 +0100

    Updated plotter server

--- service/plotter/server.py
+++ service/plotter/server.py
@@ -10,9 +10,8 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("<p>Plotter Engine endpoint</p>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 
