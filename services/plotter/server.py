0805ed45503cdeb10adb21852a5f96a26e075d44
Professor K <43756789+MrToino@users.noreply.github.com>
2022-07-11 20:26:10 +0100

    Updated both fitter and plotter servers (#9)
    Looking for some consistency between services' servers. 
    Added POST operator to have a mock for frontend

--- services/plotter/server.py
+++ services/plotter/server.py
@@ -5,14 +5,24 @@ serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
+    def _set_headers(self, content_length):
+        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-Length", content_length)
+        self.end_headers()
+
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>Plotter Engine endpoint</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
+        self._set_headers("0")
+
+    def do_POST(self):
+        content_length = self.headers['content-length']
+        length = int(content_length[0]) if content_length else 0
+
+        data_string = self.rfile.read(length)
+
+        self.send_response(200)
+        self._set_headers(str(len(data_string)))
+        self.wfile.write(data_string)
 
 
 if __name__ == "__main__":
