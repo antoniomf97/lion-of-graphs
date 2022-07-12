d387a04089c451c6ee4b1733b7936b62388c4fa5
António Fernandes <antonio.mf97@gmail.com>
2022-07-12 23:31:56 +0100

    Updated reader to convert base64 data input to panda dataframe

--- services/plotter/reader.py
+++ services/plotter/reader.py
@@ -1,20 +1,30 @@
 import base64
-
-"""
-input:
-X,Y
-1,2
-2,4
-3,8
-4,16
-"""
-
-def decode(encoded_data):
-    """Decodes"""
-    return base64.b64decode(encoded_data).decode('utf-8')
+import binascii
+import pandas as pd
+from io import StringIO
 
 
-if __name__ == '__main__':
-    b64_data = "WCxZCjEsMgoyLDQKMyw4CjQsMTY="
+def base64_to_string(b64_data):
+    """Decodes base64 encoded data to string with utf-8 encoding"""
+    try:
+        return base64.b64decode(b64_data).decode('utf-8')
+    except binascii.Error:
+        print("Invalid base64 encoding")
+        exit()
+    except UnicodeDecodeError:
+        print("Invalid utf-8 encoding")
+        exit()
 
-    print(decode(b64_data))
+
+def string_to_dataframe(string_data):
+    """Defines pandas dataframe from decoded data"""
+    try:
+        return pd.read_csv(StringIO(string_data), sep=",")
+    except ValueError:
+        print("Unable to parse column: invalid data format")
+        exit()
+
+
+def base64_to_dataframe(b64_data):
+    """Converts base64 data to pandas dataframe"""
+    return string_to_dataframe(base64_to_string(b64_data))
