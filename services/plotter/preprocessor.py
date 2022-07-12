44af463513e23e39ff96053a6273363a2d84879f
António Fernandes <antonio.mf97@gmail.com>
2022-07-12 23:32:14 +0100

    Renamed pre-processor.py to preprocessor.py

--- services/plotter/preprocessor.py
+++ services/plotter/preprocessor.py
@@ -1,16 +1,18 @@
 from reader import *
-from plotter import *
 
 """
 input:
-  X,Y
-0 1,2
-1 2,4
-2 3,8
-3 4,16
+X,Y
+1,2
+2,4
+3,8
+4,16
 """
 
 def plotter(b64_data):
-    response = base64_to_dataframe(b64_data)
-    plot(response)
-    return response
+    print(base64_to_dataframe(b64_data))
+
+
+if __name__ == '__main__':
+    plotter("WCxZCjEsMgoyLDQKMyw4CjQsMTY=")
+
