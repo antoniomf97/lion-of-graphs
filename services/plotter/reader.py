c3f0f00fb907ec6041a80ecd77da6d6e9c2283de
António Fernandes <antonio.mf97@gmail.com>
2022-07-14 22:29:51 +0100

    Resolved conflict

--- services/plotter/reader.py
+++ services/plotter/reader.py
@@ -1,30 +1,20 @@
 import base64
-import binascii
-import pandas as pd
-from io import StringIO
+
+"""
+input:
+X,Y
+1,2
+2,4
+3,8
+4,16
+"""
+
+def decode(encoded_data):
+    """Decodes"""
+    return base64.b64decode(encoded_data).decode('utf-8')
 
 
-def base64_to_string(b64_data):
-    """Decodes base64 encoded data to string with utf-8 encoding"""
-    try:
-        return base64.b64decode(b64_data).decode('utf-8')
-    except binascii.Error:
-        print("Invalid base64 encoding")
-        exit()
-    except UnicodeDecodeError:
-        print("Invalid utf-8 encoding")
-        exit()
+if __name__ == '__main__':
+    b64_data = "WCxZCjEsMgoyLDQKMyw4CjQsMTY="
 
-
-def string_to_dataframe(string_data):
-    """Defines pandas dataframe from decoded data"""
-    try:
-        return pd.read_csv(StringIO(string_data), sep=",", index_col=0)
-    except ValueError:
-        print("Unable to parse column: invalid data format")
-        exit()
-
-
-def base64_to_dataframe(b64_data):
-    """Converts base64 data to pandas dataframe"""
-    return string_to_dataframe(base64_to_string(b64_data))
+    print(decode(b64_data))
