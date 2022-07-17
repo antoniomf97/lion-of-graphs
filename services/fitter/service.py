89d88417744acbc9a815fc7d300e3ad925ae3d3e
António Fernandes <antonio.mf97@gmail.com>
2022-07-17 01:09:13 +0100

    Refactored fitter and service

--- services/fitter/service.py
+++ services/fitter/service.py
@@ -1,15 +1,43 @@
 import json
-from preprocessor import plotter
+import jsonschema
+from preprocessor import preprocess_data
+from fitter import fitter
+
+
+requestSchema = {
+    "type": "object",
+    "properties": {
+        "ContentB64": {"type": "string"}
+    }
+}
 
 
 def validate_json(request):
-    pass
+    """Validates request JSON schema"""
+    try:
+        jsonschema.validate(instance=request, schema=requestSchema)
+    except jsonschema.exceptions.ValidationError:
+        print("Invalid request JSON schema.")
+        return False
+    return True
+
+
+def parse_json(request):
+    """Validates and parse JSON request"""
+    try:
+        return json.loads(request)
+    except ValueError:
+        print("Cannot parse request JSON.")
+        exit()
 
 
 def service(request):
-    validate_json(request)
+    """Triggers the fitter engine for the given request"""
+    response = None
+
     parsed = json.loads(request)
-    response = plotter(parsed["filename"]).to_json().encode()
+    if validate_json(request):
+        data = preprocess_data(parsed["ContentB64"])
+        response = fitter(data)
 
-    return response
-
+    return response.to_json().encode()
