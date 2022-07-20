94133a1a1729bf8e2c1db8b7601a4736d4781df0
António Fernandes <antonio.mf97@gmail.com>
2022-07-20 23:21:58 +0100

    Updated modules

--- services/modules/__init__.py
+++ services/modules/__init__.py
@@ -1,3 +1,4 @@
 from services.modules.log_module import config_logger
 from services.modules.log_module import logger
 from services.modules.json_validation import validate_request
+from services.modules.preprocessor import preprocess_data
