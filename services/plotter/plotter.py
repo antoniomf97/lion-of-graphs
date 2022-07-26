c61362ce912b3d8ec2b777f13075e36594a67fb4
António Fernandes <antonio.mf97@gmail.com>
2022-07-27 00:29:58 +0100

    Integrated plotter service in fitter engine (poc)

--- services/plotter/plotter.py
+++ services/plotter/plotter.py
@@ -1,5 +1,6 @@
 from ext_modules import logger
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plotter(data):
@@ -9,3 +10,16 @@ def plotter(data):
 
     logger.debug("Show resulting plot.")
     plt.show()
+
+
+def plotter_for_fitter(data_x, data_y, function):
+    increment = (max(data_x) - min(data_x)) / 100
+    x = np.arange(min(data_x), max(data_x) + increment, increment)
+
+    logger.debug("Building plot for given data.")
+    plt.plot(data_x, data_y, 'o', label='data')
+    plt.plot(x, function(x), '-', label='fit')
+    plt.legend()
+
+    logger.debug("Show resulting plot.")
+    # plt.show()
