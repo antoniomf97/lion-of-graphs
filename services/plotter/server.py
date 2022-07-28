b1904a3ef86dc09f617418331bbe06dd6d7340df
António Fernandes <antonio.mf97@gmail.com>
2022-07-29 00:16:05 +0100

    Added invoker to call services

--- services/plotter/server.py
+++ services/plotter/server.py
@@ -46,6 +46,7 @@ if __name__ == "__main__":
     try:
         webServer.serve_forever()
     except KeyboardInterrupt:
+        print("Server interrupted.")
         pass
 
     webServer.server_close()
