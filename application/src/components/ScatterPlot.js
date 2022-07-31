49f0d8a9330c660f7036ec2174263f1ff216235c
luisferreira32 <luis.pmm.ferreira@gmail.com>
2022-07-31 14:32:11 +0100

    Paths graph

--- application/src/components/ScatterPlot.js
+++ application/src/components/ScatterPlot.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './Graph.css';
+import './Plot.css';
 
-// https://developer.mozilla.org/en-US/docs/Web/SVG/Tutorial/Paths
-export default class Graph extends React.Component {
+export default class ScatterPlot extends React.Component {
   render() {
     return (
         <div>
-            <p>Scatter graph for now</p>
             <svg className="axis" height={500} width={500}>
                 {this.props.points.map((point) => <circle cx={point.x} cy={500-point.y} r={3}/>)}
             </svg>
