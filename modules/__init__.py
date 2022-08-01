6e35aeff0205ec60f0047420cd8c2ad19dad3e25
António Fernandes <antonio.mf97@gmail.com>
2022-08-01 14:03:29 +0100

    Refactored modules incomplete

--- modules/__init__.py
+++
@@ -1,1 +0,0 @@
-from modules.preprocessor import preprocess_data
--- modules/preprocessor.py
+++
@@ -1,9 +0,0 @@
-from ext_modules.reader import base64_to_dataframe
-from ext_modules.validator import validate_data
-
-
-def preprocess_data(b64_data):
-    """Decodes, converts and validates input data"""
-    data = base64_to_dataframe(b64_data)
-    validate_data(data)
-    return data
