2871da13f7f9bf82a5e69ba60a7452ead42eb37a
António Fernandes <antonio.mf97@gmail.com>
2022-08-02 23:32:37 +0100

    Refactored error module to include new server module

--- intmodules/__init__.py
+++ intmodules/__init__.py
@@ -1,4 +1,5 @@
-from intmodules.log_module import config_logger, logger
+from intmodules.log import config_logger
+from intmodules.log import logger
 from intmodules.preprocessor import preprocess_data
 from intmodules.json_validation import validate_request
 from intmodules.server_handler import MPBRequestHandler
