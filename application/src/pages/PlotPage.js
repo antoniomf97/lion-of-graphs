2591596494e85ce7ceb597ba5083e57e062e3968
luisferreira32 <43647015+luisferreira32@users.noreply.github.com>
2022-09-04 11:24:49 +0100

    Move into loading images rather than building graphs (#18)
    Internet is fast, and we don't really care about building
    graphs for now, we just want images!

--- application/src/pages/PlotPage.js
+++ application/src/pages/PlotPage.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import ScatterPlot from '../components/ScatterPlot';
-import PathPlot from '../components/PathPlot';
+import Plot from '../components/Plot';
 import './PlotPage.css';
 
 
 export default class PlotPage extends React.Component {
   constructor(props){
     super(props);
-    // the default points should be empty
     this.state = {
-      points: [{x: 10, y: 10},{x: 50, y: 100},{x: 200, y: 50}, {x: 300, y: 200}, {x: 350, y: 200}, {x: 480, y: 250}],
-      paths: [{d:"M 10 10 C 20 20, 40 20, 50 10", stroke:"black", fill:"transparent"}, {d:"M 70 10 C 70 20, 110 20, 110 10", stroke:"black", fill:"transparent"}],
-      plotType: "paths",
+      picture: 'hack-813290_960_720.jpg',
     }
     
     this.handleOnChange = this.handleOnChange.bind(this);
     this.handleOnSubmit = this.handleOnSubmit.bind(this);
-    this.switchPlot = this.switchPlot.bind(this);
   }
 
   async handleOnChange(e) {
@@ -29,7 +24,7 @@ export default class PlotPage extends React.Component {
         const formData = new FormData();
         formData.append('file', this.state.file)
         const response = await fetch('http://localhost:8080', { // FIXME: don't have hardcoded URLs
-          method: "POST",
+          method: 'POST',
           headers: {
             'Content-type': 'multipart/form-data',
             'Access-Control-Allow-Origin': '*' // FIXME: this is to go around localhost issues
@@ -37,18 +32,7 @@ export default class PlotPage extends React.Component {
           body: formData,
         })
         const data = await response.json();
-        this.setState({ points: data})
-    }
-  }
-
-  switchPlot = () => {
-    switch (this.state.plotType) {
-      case "points":
-        return <ScatterPlot points={this.state.points} />
-      case "paths":
-        return <PathPlot points={this.state.paths} />
-      default:
-        return null
+        this.setState({ picture: data})
     }
   }
 
@@ -56,11 +40,11 @@ export default class PlotPage extends React.Component {
     return (
       <div>        
         <form onSubmit={this.handleSubmit}>
-          <input type={"file"} accept={".csv"} onChange={this.handleOnChange}/>
+          <input type={'file'} accept={'.csv'} onChange={this.handleOnChange}/>
           <button onClick={this.handleOnSubmit}> IMPORT CSV</button>
         </form>
         <div className='basePage'>
-          {this.switchPlot()}
+          <Plot src={this.state.picture}/>
         </div>
       </div>
     );
