aebaf21fe78dbdb99bcb15e60804e9aba1116b53
António Fernandes <antonio.mf97@gmail.com>
2023-01-21 13:20:04 +0000

    Removed unnecessary comments

--- services/fitter/fitter.py
+++ services/fitter/fitter.py
@@ -28,27 +28,3 @@ def LinearRegression(x: float, m: float, c: float) -> float:
 def QuadraticRegression(x: float, a: float, b: float, c: float) -> float:
     """Computes the linear regression at point x, for given parameteres"""
     return a * x * x + b * x + c
-
-
-# keep for later usage
-
-# def test_plot_linear(xdata, ydata, parameters):
-#     """Testing plot"""
-#     increment = (max(xdata) - min(xdata)) / 100
-#     x = np.arange(min(xdata), max(xdata) + increment, increment)
-
-#     fit_y = LinearRegression(x, parameters[0], parameters[1])
-#     plt.plot(xdata, ydata, "o", label="data")
-#     plt.plot(x, fit_y, "-", label="fit")
-#     plt.legend()
-
-
-# def test_plot_quadratic(xdata, ydata, parameters):
-#     """Testing plot"""
-#     increment = (max(xdata) - min(xdata)) / 100
-#     x = np.arange(min(xdata), max(xdata) + increment, increment)
-
-#     fit_y = QuadraticRegression(x, parameters[0], parameters[1], parameters[2])
-#     plt.plot(xdata, ydata, "o", label="data")
-#     plt.plot(x, fit_y, "-", label="fit")
-#     plt.legend()
