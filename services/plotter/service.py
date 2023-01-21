79cacb52756cc4f98dd68d8b883040fea918d78b
António Fernandes <43756789+MrToino@users.noreply.github.com>
2023-01-21 13:42:29 +0000

    Refactor fitter engine in a comparison with the plotter (#44)
    Refactored fitter engine in comparison with the previously implemented plotter.

--- services/plotter/service.py
+++ services/plotter/service.py
@@ -17,15 +17,27 @@ def service(data: DataFrame, options: Options) -> bytes:
     logger.debug("Preprocessing input data.")
     data = validate_dataframe(data)
 
+    logger.debug("Building plot for given inputs.")
+    plot = build_plot(data, options)
+
+    return plot
+
+
+def build_plot(data, options):
     uid = uuid4()
     fig: Figure = figure(uid)
     axes: Axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
 
-    axes.plot(data.index.values, data[data.keys()[0]].values, color=options.color)
+    xdata = data.index.values
+    ydata = data[data.keys()[0]].values
+
+    axes.plot(xdata, ydata, color=options.color)
+    axes.scatter(xdata, ydata, color="red")
     set_configurations(axes, options)
 
     buf = BytesIO()
     fig.savefig(buf, format="png")
+
     return buf.getvalue()
 
 
